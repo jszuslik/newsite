879750d00241594d56d341ed1f784868922574eb
Joshua Szuslik <joshuaszuslik@gmail.com>
2018-03-15 23:03:42 -0500

    Update

--- core/customizer/header/NrwHeaderHooks.php
+++ core/customizer/header/NrwHeaderHooks.php
@@ -130,7 +130,7 @@ class NrwHeaderHooks {
                                     <?php } ?>
                                 </div>
                                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                                    <span class="navbar-toggler-icon"></span>
+                                    <i class="fa fa-bars"></i>
                                 </button>
 	                            <?php
 	                            wp_nav_menu(
